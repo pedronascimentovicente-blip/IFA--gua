ea7b74742e16a3b53601f687daca8985fca50079
pedronascimentovicente-blip <pedro.nascimento.vicente@escola.pr.gov.br>
2026-07-28 11:02:07 -0300

    1.0

---
+++ style.css
@@ -0,0 +1,3 @@
+// Atualmente a interface funciona puramente com HTML/CSS estrutural.
+// Você pode utilizar este arquivo no futuro para carregar transições de dados dinâmicas.
+console.log("Interface do Portal de Recursos Hídricos carregada com sucesso.");
